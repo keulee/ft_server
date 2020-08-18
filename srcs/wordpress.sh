754011df34d38bc6d442feb168f94a032fe6761d
Keungeun LEE <keulee42@gmail.com>
2020-08-18 18:08:42 +0200

    ft_server clean

--- srcs/wordpress.sh
+++ srcs/wordpress.sh
@@ -1,6 +1,5 @@
 wget https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
-chown -R www-data:www-data wordpress
-chmod -R 755 wordpress
+chown -R root:root wordpress
 cp /tmp/wp-config.php wordpress
 rm ./wordpress/wp-config-sample.php
